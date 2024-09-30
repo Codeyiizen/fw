5c34ceb9f2441a9f941417f6b90f11aafa52fd41
mahendracodeyiizen <129163349+mahendracodeyiizen@users.noreply.github.com>
2024-09-30 11:22:52 +0530

    latest code commit
    latest code commit

--- application/views/front/template/template_footer.php
+++ application/views/front/template/template_footer.php
@@ -1,38 +1,50 @@
 </main>
 <footer>
-	<div class="main-footer">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-lg-8">
-					<div class="footer-inner text-center">
-						<div class="social-media mb-5">
-							<ul class="d-flex justify-content-center">
-								<li><a href="https://twitter.com/favoritewishltd" class=""><img src="<?php echo base_url(); ?>assets/images/x-social-media-round-icon.png" alt=""></a></li>
-								<li><a href="https://www.instagram.com/favoritewishltd?igsh=ZW5ocmRxcHY2bnRk&utm_source=qrClubhouse:" class=""><img src="<?php echo base_url(); ?>assets/images/ig-instagram-icon.png" alt=""></a></li>
-								<li><a href="https://www.clubhouse.com/@favoritewish?utm_medium=ch_profile&utm_campaign=Dyi26k477x_bI9VN65Qdbg-1123563" class=""><img src="<?php echo base_url(); ?>assets/images/clubhouse-icon-1.png" alt=""></a></li>
-								<li><a href="https://www.threads.net/@favoritewishltd" class=""><img src="<?php echo base_url(); ?>assets/images/threads-app-icon.png" alt=""></a></li>
-							</ul>
-						</div>
-						<p>All rights reserved to Favorite Wish 2023</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="bottom-footer background-image p-3" data-background="<?php echo base_url(); ?>assets/images/site-image/footer-bg.png">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-9 col-sm-12 text-center text-md-left">
-					<div class="footer-menu-list">
-						<a href="<?php echo base_url('/terms-and-conditions'); ?>">Terms and Conditions</a>
-						<a href="<?php echo base_url(); ?>privacy/Policy">Privacy Policy</a>
-						<a href="<?php echo base_url('/front/company/policy'); ?>">Company Relations</a>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-12 text-center text-md-right">
-					<p>Designed by <a href="https://www.kurieta.com">Kurieta.com</a></p>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="main-footer">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="footer-inner text-center">
+                        <div class="social-media mb-5">
+                            <ul class="d-flex justify-content-center">
+                                <li><a href="https://twitter.com/favoritewishltd" class=""><img
+                                            src="<?php echo base_url(); ?>assets/images/x-social-media-round-icon.png"
+                                            alt=""></a></li>
+                                <li><a href="https://www.instagram.com/favoritewishltd?igsh=ZW5ocmRxcHY2bnRk&utm_source=qrClubhouse:"
+                                        class=""><img src="<?php echo base_url(); ?>assets/images/ig-instagram-icon.png"
+                                            alt=""></a></li>
+                                <li><a href="https://www.clubhouse.com/@favoritewish?utm_medium=ch_profile&utm_campaign=Dyi26k477x_bI9VN65Qdbg-1123563"
+                                        class=""><img src="<?php echo base_url(); ?>assets/images/clubhouse-icon-1.png"
+                                            alt=""></a></li>
+                                <li><a href="https://www.threads.net/@favoritewishltd" class=""><img
+                                            src="<?php echo base_url(); ?>assets/images/threads-app-icon.png"
+                                            alt=""></a></li>
+                            </ul>
+                        </div>
+                        <p>All rights reserved to Favorite Wish 2023</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom-footer background-image p-3"
+        data-background="<?php echo base_url(); ?>assets/images/site-image/footer-bg.png">
+        <div class="container">
+            <div class="row d-flex justify-content-between align-items-center">
+                <div class="col-md-6 col-sm-12 text-center text-md-left mb-3 mb-md-0">
+                    <div class="footer-menu-list d-flex flex-column flex-md-row">
+                        <a href="<?php echo base_url('/terms-and-conditions'); ?>" class="mr-md-3 mb-2 mb-md-0">Terms
+                            and Conditions</a>
+                        <a href="<?php echo base_url(); ?>privacy/Policy" class="mr-md-3 mb-2 mb-md-0">Privacy
+                            Policy</a>
+                        <a href="<?php echo base_url('/front/company/policy'); ?>" class="mr-md-3 mb-2 mb-md-0">Company
+                            Relations</a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12 text-center text-md-right">
+                    <p>Designed by <a href="https://www.kurieta.com">Kurieta.com</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
